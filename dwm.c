97b3d81b489481d4a4842c05057bea8c4e1900bb
lizqwerscott <lizqwerscott@163.com>
2021-06-26 10:01:25 +0800

    change .dwm to scripts

--- dwm.c
+++ dwm.c
@@ -1515,8 +1515,8 @@ run(void)
 
 void
 runAutostart(void) {
-	system("cd ~/.dwm; ./autostart_blocking.sh");
-	system("cd ~/.dwm; ./autostart.sh &");
+	system("cd ~/scripts; ./autostart_blocking.sh");
+	system("cd ~/scripts; ./autostart.sh &");
 }
 
 void
