4a69c20d0be6e6aad2ecd64035f168698c4bb359
lizqwerscott <lizqwerscott@163.com>
2023-06-07 10:21:55 +0800

    add rofi

--- config.h
+++ config.h
@@ -66,6 +66,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *roficmd[]  = { "rofi", "-show", "drun", NULL };
+static const char *rofiwindowcmd[]  = { "rofi", "-show", "window", NULL };
+static const char *rofisshcmd[]  = { "rofi", "-show", "ssh", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *trayercmd[] = {"/home/lizqwer/scripts/t-toggle.sh", NULL };
 static const char *browsercmd[] = {"google-chrome-stable", NULL};
@@ -74,7 +77,9 @@ static const char *emacscmd[] = {"emacs", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
+    { MODKEY|ShiftMask,             XK_p,      spawn,          {.v = rofiwindowcmd } },
+    { MODKEY,                       XK_n,      spawn,          {.v = rofisshcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
     { MODKEY|ShiftMask,             XK_t,      spawn,          {.v = trayercmd } },
 	{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = browsercmd } },
